a9d8122424908d64d18e5e1b549d9ea32e9072a5
August Knecht <augustknecht@gmail.com>
2010-01-20 23:21:17 -0600

    working, but not optimally. making it actually do something now would be cool

--- motion_detector.rb
+++ motion_detector.rb
@@ -1,12 +1,19 @@
 class MotionDetector < ArduinoSketch
   
   input_pin 0, :as => :sensor
+  output_pin 7, :as => :motion
   
   serial_begin :rate => 9600
   
   def loop
-    serial_println analogRead sensor
-    delay 200
+    motion_in = analogRead sensor
+    serial_println motion_in
+    if (motion_in > 500)
+      digitalWrite(motion, true)
+    else
+      digitalWrite(motion, false)
+    end
+    delay 100
   end
     
 end
